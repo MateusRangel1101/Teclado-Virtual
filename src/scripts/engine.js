20a09b25d6c9323086ae30153db6f58060668aa3
Mateus Rangel <mateusrangel.com.br@gmail.com>
2023-11-29 20:35:27 -0300

    mapeando teclas tocadas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key')
 
 let audio = new Audio(`./src/tunes/a.wav`)
+let mapedKeys = []
 
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`
@@ -17,8 +18,11 @@ const playTune = (key) => {
 pianoKeys.forEach((key) => {
   key.addEventListener('click',
     () => playTune(key.dataset.key))
+  mapedKeys.push(key.dataset.key)
 })
 
 document.addEventListener('keydown', (e) => {
-  playTune(e.key)
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key)
+  }
 })
